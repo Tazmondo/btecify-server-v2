375ba399df4b138fc7318a958fe0d0743bba4ac0
Tazmondo <fondcoolman@outlook.com>
2022-04-16 20:51:07 +0100

    moved db file some basic api code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, File
+from sqlalchemy.orm import Session
+
+import app.models as models
+from app.db import SessionLocal
 
 app = FastAPI()
+
+
+# Dependency
+def getdb():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -8,6 +21,8 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get('/api/songsource')
+async def getSongSource(songId: int, db: Session = Depends(getdb)):
+    dbSong: models.Song = db.query(models.Song).get(songId)
+    if not dbSong:
+        raise HTTPException(404, "Song not found")
